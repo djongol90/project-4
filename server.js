cb4c95b003f96d0db2ae68ca68872a3cbf303044
ibrahimDaoud12 <ibrahimdaoudhieesin39@gmail.com>
2024-02-16 20:19:23 +0100

    Apres initial:commit

--- server.js
+++ server.js
@@ -3,11 +3,12 @@ const mongoose = require("mongoose");
 
 mongoose.connect('mongodb://127.0.0.1:27017/test42');
 
-// const Cat = mongoose.model('Cat', {name: String});
+const Cat = mongoose.model('Cat', {name: String});
 const Chien = mongoose.model('Chien', {name: String, category: String});
-
-// const newChien = new Chien({name: "Dogg 2", category: "Afro"});
-// // console.log(newChien);
+const Personne = mongoose.model('Personne',{Nom :String, Prenom:String, Age:Number})
+const Anne = mongoose.model('Anne',{name: String, prenom : String, category: String});
+const newChien = new Chien({name: "Dogg 5", category: "Hindien"});
+console.log(newChien);
 
 // La fonction find() il return la liste de toutes les informations qui se trouve
 // au niveau de la table Chien
@@ -20,14 +21,20 @@ const Chien = mongoose.model('Chien', {name: String, category: String});
 // La fonction findOne() il return un seul object
 // mais en passe en argument l'attribut qu'on veux filtrer avec
 // exple findOne({name: "le nom qu'on veux rechercher avec"}); 
-Chien.findOne({name: "Dogg 2"}).then(data => console.log(data))
+Chien.findOne({name: "Dogg 5"}).then(data => console.log(data))
+newChien.save().then(() => console.log('Le chien a ete ajouté'))
 
-// newChien.save().then(() => console.log('Le chien a ete ajouté'))
 
-// const newCat = new Cat({name: 'Mioooioo'});
+const newPersonne = new Personne({Nom:"Ibrahim", Prenom:"Daoud", Age:22})
+console.log(newPersonne)
+newPersonne.save().then(() => console.log('la personne a ete bien ajouter'))
 
-// newCat.save().then(() => console.log("chat ajouté"));
+const newCat = new Cat({name: 'Boby'});
+const newAnne = new Anne({name : "Houmar 2", Prenom: "Quartier", category: "Dahache"})
+console.log(newAnne)
+newAnne.save().then(() => console.log("l' anne est ajouter"))
+newCat.save().then(() => console.log("chat ajouté"));
 
-// Cat.find().then((data) => {
-//     console.log(data);
-// })
+Cat.find().then((data) => {
+    console.log(data);
+})
