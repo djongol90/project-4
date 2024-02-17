5b376addd1a172abb668b4d1992c7ec569fc9831
Haggar99 <amadydjongol@gmail.com>
2024-02-17 17:24:51 +0100

    feat: implementation des fonction mongoose

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const mongoose = require('mongoose');
 const express = require('express');
 const bodyParser = require('body-parser');
+const morgan = require('morgan')
 
 const app = express();
+
+app.use(morgan('dev'));
 
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
@@ -35,18 +38,42 @@ app.post('/add-person', (req, res) => {
 });
 
 app.get('/persons', (req, res) => {
-
-
     Person.find().then(data => {
         res.json({
             message: 'La liste des personne',
             personnes: data
         })
     })
+});
+
+
+app.get('/person/:id', (req, res) => {
+    const id = req.params.id;
+    Person.findById(id).then(data => {
+        res.json({
+            message: 'La personnne a été retrouvée!',
+            personne: data
+        })
+    })
 })
 
 
+app.get('/person/get-one/:email', async (req, res) => {
+    const email = req.params.email;
+    const person = await Person.findOne({email: email})
+    res.json({
+        message: 'La personne a été retrouvee!',
+        person: person
+    })
+})
 
+app.put('/person/update-person/:email', async(req, res) => {
+    const email = req.params.email;
+    await Person.updateOne({email: email}, {name: req.body.name});
+    res.json({
+        message: "la personne a ete modifiée"
+    })
+})
 
 
 
